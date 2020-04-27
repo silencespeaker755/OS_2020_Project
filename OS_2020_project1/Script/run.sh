56bb77a0804e7b850c7a312826bc2bdfca4c465e
silencespeaker <b06902019@ntu.edu.tw>
2020-04-27 21:05:22 +0800

    update run.sh

--- OS_2020_project1/Script/run.sh
+++ OS_2020_project1/Script/run.sh
@@ -3,7 +3,11 @@ for scheduling in FIFO SJF PSJF RR; do
 	for i in {1..5};
 	do
 		sudo dmesg --clear;
-		./main < ../OS_PJ1_Test/${scheduling}_${i}.txt > output1/${scheduling}_${i}_stdout.txt;
-		dmesg | grep Project1 > output1/${scheduling}_${i}_dmesg.txt;
+		./OS_project < ./OS_PJ1_Test/${scheduling}_${i}.txt > outputs/${scheduling}_${i}_stdout.txt;
+		dmesg | grep Project1 > outputs/${scheduling}_${i}_dmesg.txt;
 	done;
 done;
+
+sudo dmesg --clear;
+./OS_project < ./OS_PJ1_Test/TIME_MEASUREMENT.txt > outputs/TIME_MEASUREMENT_stdout.txt;
+dmesg | grep Project1 > outputs/TIME_MEASUREMENT_dmesg.txt;
